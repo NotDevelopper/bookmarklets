30d51ec5c849e6ed5e3f41f30a6bd1b2f2b7be11
Alex Semёnov <84786361+NotDevelopper@users.noreply.github.com>
2024-02-06 19:57:16 +0700

    Update Yandex.Translate (new tab).js
    translate a page in a new tab

--- Yandex.Translate (new tab).js
+++ Yandex.Translate (new tab).js
@@ -1,3 +1,5 @@
+// This script translates the current page using Yandex.Translate. The result opens in a new tab.
+// Copy it, delete all Enter and Space and save it as a bookmark in your browser.
 javascript:
 q=(document.location.href);
 void(
